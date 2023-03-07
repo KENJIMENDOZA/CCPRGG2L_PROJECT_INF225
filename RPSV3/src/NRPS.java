d8bb4e84cc7e6bcb8e9eb8e7c73810dbefa02efa
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2023-03-07 22:05:27 +0800

    NRPS
    fixed computer always win bug

--- RPSV3/src/NRPS.java
+++ RPSV3/src/NRPS.java
@@ -1,20 +1,22 @@
-import javax.crypto.spec.RC5ParameterSpec;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.security.interfaces.RSAPrivateCrtKey;
 
-public class RPS extends JFrame implements ActionListener {
+
+
+public class NRPS extends JFrame implements ActionListener {
     private JButton rockButton;
     private JButton paperButton;
     private JButton scissorsButton;
+    private JButton spockButton;
     private JLabel resultLabel;
 
-    public RPS() {
+    public NRPS() {
         // Create the buttons and add an action listener to each as well as adding fonts and designs(feel free to reconfigure just remember to tell before you commit)
-        rockButton = new JButton("Rock");
+        rockButton = new JButton("rock");
         rockButton.addActionListener(this);
-        rockButton.setIcon(new ImageIcon("TR.png"));
+        rockButton.setIcon(new ImageIcon("rock.png"));
         rockButton.setBounds(getBounds());
         rockButton.setSize(25, 25);
         rockButton.setBounds(50,50,50,50);
@@ -23,11 +25,11 @@ public class RPS extends JFrame implements ActionListener {
         rockButton.setVerticalTextPosition(JButton.BOTTOM);
         rockButton.setBorder(BorderFactory.createEtchedBorder());
         rockButton.setFocusable(false);
-        rockButton.setLayout(null);
+        
 
-        paperButton = new JButton("Paper");
+        paperButton = new JButton("paper");
         paperButton.addActionListener(this);
-        paperButton.setIcon(new ImageIcon("TP.png"));
+        paperButton.setIcon(new ImageIcon("paper.png"));
         paperButton.setSize(25, 25);
         paperButton.setBounds(50,50,50,50);
         paperButton.setFont(new Font("Comic Sans", Font.BOLD,25));
@@ -35,12 +37,12 @@ public class RPS extends JFrame implements ActionListener {
         paperButton.setVerticalTextPosition(JButton.BOTTOM);
         paperButton.setBorder(BorderFactory.createEtchedBorder());
         paperButton.setFocusable(false);
-        paperButton.setLayout(null);
+  
 
 
-        scissorsButton = new JButton("Scissors");
+        scissorsButton = new JButton("scissors");
         scissorsButton.addActionListener(this);
-        scissorsButton.setIcon(new ImageIcon("Hand.png"));
+        scissorsButton.setIcon(new ImageIcon("scissor.png"));
         scissorsButton.setSize(25, 25);
         scissorsButton.setBounds(50,50,50,50);
         scissorsButton.setFont(new Font("Comic Sans", Font.BOLD,25));
@@ -48,17 +50,30 @@ public class RPS extends JFrame implements ActionListener {
         scissorsButton.setVerticalTextPosition(JButton.BOTTOM);
         scissorsButton.setBorder(BorderFactory.createEtchedBorder());
         scissorsButton.setFocusable(false);
-        scissorsButton.setLayout(null);
+
+        spockButton = new JButton("spock");
+        spockButton.addActionListener(this);
+        spockButton.setIcon(new ImageIcon("spock.png"));
+        spockButton.setSize(25, 25);
+        spockButton.setBounds(50,50,50,50);
+        spockButton.setFont(new Font("Comic Sans", Font.BOLD,25));
+        spockButton.setHorizontalTextPosition(JButton.CENTER);
+        spockButton.setVerticalTextPosition(JButton.BOTTOM);
+        spockButton.setBorder(BorderFactory.createEtchedBorder());
+        spockButton.setFocusable(false);
+    
 
         // Create the result label
         resultLabel = new JLabel();
 
         // Add the buttons and result label to the content pane
         Container c = getContentPane();
-        c.setLayout(new GridLayout(2, 1));
+        c.setLayout(new FlowLayout());
+        c.setSize(250, 1500);
         c.add(rockButton);
         c.add(paperButton);
         c.add(scissorsButton);
+        c.add(spockButton);
         c.add(resultLabel);
 
         // Set the window properties
@@ -72,21 +87,23 @@ public class RPS extends JFrame implements ActionListener {
         // Determine which button was clicked
         if (e.getSource() == rockButton) {
             // Set the result label to display the rock icon
-            resultLabel.setIcon(new ImageIcon("TR.png"));
+            resultLabel.setIcon(new ImageIcon("rock.png"));
         } else if (e.getSource() == paperButton) {
             // Set the result label to display the paper icon
-            resultLabel.setIcon(new ImageIcon("TP.png"));
+            resultLabel.setIcon(new ImageIcon("paper.png"));
         } else if (e.getSource() == scissorsButton) {
             // Set the result label to display the scissors icon
-            resultLabel.setIcon(new ImageIcon("Hand.png"));
+            resultLabel.setIcon(new ImageIcon("scissor.png"));
+        }else if (e.getSource() == spockButton) {
+            // Set the result label to display the spock icon
+            resultLabel.setIcon(new ImageIcon("spock.png"));
+        
         }
-        
-        
         String playerChoice = e.getActionCommand();
 
         
         // generate computer's choice
-        String[] choices = {"rock", "paper", "scissors"};
+        String[] choices = {"rock", "paper", "scissors","spock"};
         int computerIndex = (int)(Math.random() * choices.length);
         String computerChoice = choices[computerIndex];
 
@@ -97,19 +114,23 @@ public class RPS extends JFrame implements ActionListener {
             result = "It's a tie!";
         } else if ((playerChoice.equals("rock") && computerChoice.equals("scissors"))
                 || (playerChoice.equals("paper") && computerChoice.equals("rock"))
-                || (playerChoice.equals("scissors") && computerChoice.equals("paper"))) {
+                || (playerChoice.equals("paper") && computerChoice.equals("spock"))
+                || (playerChoice.equals("scissors") && computerChoice.equals("paper"))
+                || (playerChoice.equals("spock") && computerChoice.equals("scissors"))
+                || (playerChoice.equals("spock") && computerChoice.equals("rock"))) {
             result = "You win!";
         } else {
             result = "Computer wins!";
         }
         
+        
         // update result label
         resultLabel.setText("You chose " + playerChoice + ". Computer chose " + computerChoice + ". " + result);
         
-    }
     
+}
     public static void main(String[] args) {
-        new RPS() {
+        new NRPS() {
             
         };
     }
